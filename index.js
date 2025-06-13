4b93ed95877322fceaed3e3872093e0ae9f5dff7
Imtiaz Ahmed <imtiazali78652@gmail.com>
2025-06-13 17:42:02 +0500

    changed front-end url

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const app = express();
 
 // Allow CORS
 app.use(cors({
-    origin: 'http://localhost:3000', // replace with your frontend origin
+    origin: 'https://task-forge-front-end.onrender.com/', // replace with your frontend origin
     credentials: true
 }));
 
